56c380bf8750d79bdecaf03d00bb5e7ca176267a
Anton Goncharuk <antonge92@gmail.com>
2017-06-26 02:58:00 +0300

    babel, eslint config. Add npm scripts. Use Sass. Add webpack config. Add dependencies, update yarn lock

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './src/ClientApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.webpack.js', '.scss']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }]
+      },
+      {
+        enforce: 'pre',
+        test: /\.jsx?%/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+
+    ]
+  }
+};
